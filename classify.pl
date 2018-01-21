4f33b7ed978ba002efbc7d530b18fe210a7692e4
Anas Rchid <rchid.anas@gmail.com>
2018-01-21 12:48:52 +0000

    format the code..

--- classify.pl
+++ classify.pl
@@ -14,64 +14,77 @@
 # TODO: generate docs
 # CREATED: 01/21/2018
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
 package classify;
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# #			       pragmas
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 use strict;
 use warnings;
+use diagnostics;
 use Getopt::Long;
+use feature q{say};		# this is fucking awesome!
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# #			     sub routines
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+my @main_args = undef;
+my $topics = "";
+my $dicts = "";
 my $quiet = undef;
-my $help;
-my $topics;
-my $dicts;
-my @main_args;
+my $help = undef;
 
-sub handleoptions () {
+sub handleoptions {
      # Process options.
      if (@ARGV > 0) {
 	  GetOptions('quiet|q' => \$quiet, 'help|?'  => \$help,
 		     'dict=s'  => \$dicts, 'topic=s' => \$topics);
 	  @main_args = @ARGV;
-	  print "hell yeah!" unless $quiet;
-	  print @main_args unless $quiet;
+	  unless ($quiet) {
+	       say "not super quiet";
+	       say "dicts:\t".$dicts;
+	       say "topics:\t".$topics;
+	       say "main args:@main_args";
+	       say "\t\t\t--------------";
+	  }
+     } else {
+	  die ('no document was specified!'."\n"
+	       .'use --help or -? for more information');
      }
 }
 
-handleoptions();
 
 my $ERR_MSG = 'there was a problem while loading..';
 
 # count the appearance of words in a particular document according
 # to a certain topic or dictionary.
-sub count_in {
-     my $dict = $_[1];
+sub countin {
      my $doc  = $_[0];
+     my $dict = $_[1];
+     my $doc_count = countwords $_[0];
 
      my %count;			# per word
 }
 
 # ($login, $passwd) = split(/:/); very nice
-# count words in a document
+
+# count the number words in a document
 sub countwords {
      my $doc = $_[0];
      my $count = 0;
-     my $ERR = -1;
+     my $ERR = -1;	       # return value in case there's an error
 
-     print "\n".$doc unless $quiet;
+     say "\nin doc:\t".$doc unless $quiet;
 
      # perl has a cool built-in feature of dealing with files
      # -s: returns the files size in bytes.
      return $ERR unless -s $doc;
-     open my $in, '<', $doc or die $ERR_MSG;
 
-     while (<$in>) {
+     open my $fin, '<', $doc or die $!;
+
+     while (<$fin>) {
 	  my ($line) = $_;	# local variable
 	  unless ('#' eq substr $line, 0, 1) { # ignore comments
 	       chomp $line;
@@ -82,9 +95,21 @@ sub countwords {
      }
 
      # never forget to close the opened file
-     close $in;
+     close $fin;
 
+     say "are you sure that '$doc' is a text file?"
+	  unless $count > 0 || $quiet;
      return $count;
 }
 
-print "\n", countwords $main_args[0];
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# #			      main stuff
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+handleoptions;			# get all the options
+
+for my $i (@main_args) {
+     print "word count: ".countwords ($i)." word(s) in '".$i."'\n";
+}
+
