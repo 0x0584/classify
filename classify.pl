a3773a908c13a04b7ed2ec625d39c80fe12baee7
Anas Rchid <rchid.anas@gmail.com>
2018-01-24 00:02:03 +0000

    more hashes..

--- classify.pl
+++ classify.pl
@@ -24,7 +24,7 @@ use strict;
 use warnings;
 use diagnostics;
 use Getopt::Long;
-use feature q{say};		# this is fucking awesome!
+# use feature q{say};		# this is fucking awesome!
 
 # use topic;
 
@@ -40,86 +40,92 @@ my $quiet = "";			# no unnecessary output
 my $help = "";			# show a basic help
 
 my %MSG = (
-  ERR  => 'there was a problem while loading..',
-  # #
-  HELP => "usage: $0 foo.txt [OPTIONS..]\n".
-  "\nOPTIONS:\n\t+ [-d=1..]\n\t+ [-t=1..]\n\t+ [-i=1..]\n",
-  # #
-  NO_DOC => 'no document was specified!'."\n".
-  'use --help or -? for more information'
- );
+    ERR  => 'there was a problem while loading..',
+    # #
+    HELP => "usage: $0 foo.txt [OPTIONS..]\n".
+    "\nOPTIONS:\n\t+ [-d=1..]\n\t+ [-t=1..]\n\t+ [-i=1..]\n",
+    # #
+    NO_DOC => 'no document was specified!'."\n".
+    'use --help or -? for more information'
+);
 
-sub handle_args {
-  # Process options.
-  if (@ARGV > 0) {
-    GetOptions(
-      'quiet|q' => \$quiet,
-      'help|?'  => \$help,
-      'dict|d=s{1,}'  => \@dicts,
-      'topic|t=s{1,}' => \@topics,
-      'input|i=s{1,}' => \@input
-     );
+sub handle_args () {
+    # Process options.
+    if (@ARGV > 0) {
+	GetOptions (
+	    'quiet|q' => \$quiet,
+	    'help|?'  => \$help,
+	    'dict|d=s{1,}'  => \@dicts,
+	    'topic|t=s{1,}' => \@topics,
+	    'input|i=s{1,}' => \@input
+	);
 
-    die ($MSG{'HELP'}) if $help;
+	die ($MSG{'HELP'}) if $help;
 
-    # merging just in case of sending files without
-    # specifing -i or --input
-    @main_args = (@input, @ARGV);
+	# merging just in case of sending files without
+	# specifing -i or --input
+	@main_args = (@input, @ARGV);
 
-    unless ($quiet) {
-      say "not super quiet $quiet";
-      say "\ndicts:\t".join "\n\t", @dicts;
-      say "\ntopics:\t".join "\n\t", @topics;
-      say "\nargs:\t".join "\n\t", @main_args;
-      say "\n\t\t--------------\n";
+	unless ($quiet) {
+	    print "not super quiet $quiet\n";
+	    print "\ndicts:\t".join "\n\t@dicts\n";
+	    print "\ntopics:\t".join "\n\t@topics\n";
+	    print "\nargs:\t".join "\n\t@main_args\n";
+	    print "\n\t\t--------------\n\n";
+	}
+    } else {
+	die ($MSG{'NO_DOC'});
     }
-  } else {
-    die ($MSG{'NO_DOC'});
-  }
 }
 
 
 
 # count the appearance of words in a particular document according
 # to a certain topic or dictionary.
-sub countin {
-  my $doc  = $_[0];
-  my $dict = $_[1];
-  my $doc_count = countwords $_[0];
+# sub countin {
+#     my $doc  = $_[0];
+#     my $dict = $_[1];
+#     my $doc_count = countwords $_[0];
 
-  my %count;			# per word
-}
+#     my %count;			# per word
+# }
 
+# @description  takes a file 'foo.txt' which contains a set of words
+#		and returns it as an array
+# @param	document, a regular ASCII file
+# @return	array of words
 sub to_array {
-  my $doc = $_[0];
-  my @array;
+    my $doc = $_[0];
+    my @array;
 
-  # this is silly but, i'll figure it out later
-  if (-s $doc) {
-    open my $in, '<', $doc or (print "'$doc'\t: ".$!."\n" and goto FAILURE);
+    # this is silly but, i'll figure it out later
+    if (-s $doc) {
+	open my $in, '<', $doc or (print "'$doc'\t: .$!\n" and goto FAILURE);
 
-    while (<$in>) {
-      unless ('#' eq substr $_, 0, 1) { # ignore comments
-	chomp $_;
-	push @array, (split /\s+/, $_);
-      }
+	while (<$in>) {
+	    # ignore lines that start with a '#' as it is a standard
+	    # way to indicate a commented line.
+	    unless ('#' eq substr $_, 0, 1) {
+		chomp $_;
+		push @array, (split /\s+/, $_);
+	    }
+	}
+
+	close $in;
+    } else {
+	goto FAILURE;
     }
 
-    close $in;
-  } else {
-    goto FAILURE;
-  }
-
-  return @array;
- FAILURE:
-  return undef;
+    return @array;
+  FAILURE:
+    return undef;
 }
 
 # ($login, $passwd) = split(/:/); very nice
 
 # count the number words in a document
 sub count_words {
-  return scalar to_array $_[0];
+    return scalar to_array $_[0];
 }
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -130,7 +136,7 @@ handle_args;			# get all the args
 
 # 0.0 number of words in the documents
 for my $i (@main_args) {
-    print "'$i'\t: ".count_words($i)." word(s)\n"
+    print "'$i'\t: ", count_words($i), " word(s)\n"
 }
 
 # ==================================================================
@@ -160,42 +166,39 @@ for my $i (@main_args) {
 # ==================================================================
 #
 
-# @description this take a document and show the occurence of words in
-# topics and also its langauge level and whether it's a positive or
-# a negative one.
-#
-# @param document
-# @param list of topics
-# @param list of dictionaires
-#
-# @return array (or hashmap) of something i don't know for the moment.
+# @description	this take a document and show the occurence of words in
+#		topics and also its langauge level and whether it's a
+#		positive or a negative one.
+# @param	document
+# @param	array of topics
+# @param	array of dictionaires
+# @return	array (or hashmap) of something i don't know for the moment.
 sub doc_analysis (\$\@\@) {
-  my $doc_ref = shift;
-  my $dicts_ref = shift;
-  my $topics_ref = shift;
+    my $doc_ref = shift;
+    my $dicts_ref = shift;
+    my $topics_ref = shift;
 
-  # words appearance
-  my @topic_stats;
-  my @dict_stats;
+    # words appearance
+    my @topic_stats;
+    my @dict_stats;
 
-  # 1. get words statistics, foreach doc
-  # 1.1. figure topics
-  for my $t (@$topics_ref) {
-    my %stats;
-    for my $word (to_array $t) {
-      $stats{$word}++;
+    # 1. get words statistics, foreach doc
+    # 1.1. figure topics
+    for my $t (@$topics_ref) {
+	my %stats;
+	for my $word (to_array $t) {
+	    $stats{$word}++;
+	}
+	# find how to deal with array of hashes
+	push @topic_stats, %stats;
     }
-    # find how to deal with array of hashes
-    push @topic_stats, %stats;
-  }
 
-  # 1.2. figure language level
+    # 1.2. figure language level
 
-  # 2. sort the results
-  # 3. pick the highest one while indicating it's language level
+    # 2. sort the results
+    # 3. pick the highest one while indicating it's language level
 }
 
 for my $d (@main_args) {
-  print doc_analysis $d, @dicts, @topics;
+    print doc_analysis $d, @dicts, @topics;
 }
-
--- foo.pl
+++ foo.pl
@@ -7,6 +7,77 @@
 #   MODIFIED: 01/22/2018
 #
 
+# my @strs = qw! test shit fool cool !;
+# my ($foo, $bar) = @strs;
+
+# print "@strs\n$foo\n$bar\n";
+# print scalar @strs;
+
+# this is not a good style!
+# @strs = qw(
+# 	      sss
+# 	      sss
+# 	      sss
+#       );
+
+# @strs = (
+#     1, 2, 1,
+#     2, 1, 2,
+# );
+
+# my @range = 1..1e3;
+
+# my @foo = (@range, undef, @range);
+# print scalar @foo;
+
+# my @arr = qw! cool way to say something !;
+# print "@arr\n";
+
+# my ($last, $first) = (pop(@arr), shift(@arr));
+# print "@arr\n";
+
+# push @arr, 78;
+# unshift @arr, 89;
+
+# print "$last $first\n";
+# print "@arr\n";
+
+# splice @arr, 0, 3, qw! oh this is fucking awesome\! !;
+# print "@arr\n";
+
+# sub nary_print {		# from perldoc (on splice)
+#     my $n = shift;
+#     while (my @next_n = splice @_, 0, $n) {
+#         print join(q! -- !, @next_n), "\n";
+#     }
+# }
+
+# nary_print 3, qw/ this is cool to have fun wow /;
+# my @text = qw/ this is a test /;
+# print "@text\n";
+# foreach my $word (@text) {
+#     $word .= q/ -- /;
+# }
+
+# print "@text\n";
+
+# foreach (@text) {
+#     $_ .= q/ ** /;
+# }
+
+# print "@text\n";
+
+my @arr = reverse 1..6;
+print "@arr\n";
+
+my %hash = (
+    foo => "hah",
+    bar => "jkj"
+);
+
+print "aaa".$hash{'bar'};
+print $hash{foo};
+
 # sub something (\@\@) {
 #   my $ref0 = shift;
 #   my $ref1 = shift;
@@ -32,26 +103,64 @@
 
 # print (scalar undef);
 
-my %s;
-my %ss = {
-  s=>"sss",
-  b=>11
- };
+# my %s;
+# my %ss = {
+#   s=>"sss",
+#   b=>11
+#  };
 
-for my $foo (1,2,44,5,4,2,2,1,4,1,2) {
-  $s{$foo}++;
-}
-
-# for my $bar (keys %s) {
-#   print $bar." - ".$s{$bar}."\n";
+# for my $foo (1,2,44,5,4,2,2,1,4,1,2) {
+#   $s{$foo}++;
 # }
 
-my @arr = (%s, %ss);
+# # for my $bar (keys %s) {
+# #   print $bar." - ".$s{$bar}."\n";
+# # }
 
-for my $f (@arr) {
-  for my $s (keys %$f) {
-    print "$s - ".$f->{$s}."\n";
-  }
-}
+# my @arr = (%s, %ss);
 
+# for my $f (@arr) {
+#   for my $s (keys %$f) {
+#     print "$s - ".$f->{$s}."\n";
+#   }
+# }
+
+# my @arr = (
+#   { off => "kkk", on => "xxx" },
+#   { foo => "ssss", bar => "xxx" }
+#  );
+
+# print "sss";
+
+# print "$arr[0]{off}";
+
+# my @arr0;
+# my @arr;
+
+# my %hash0 = {
+#   foo => "vvvv",
+#   bar => "ssss"
+#  };
+# my %hash1 = {
+#   here => "oh! i am anas",
+#   there => "here's the best things"
+#  };
+
+# push @arr, %hash0;
+# push @arr, %hash1;
+
+# push @arr0, \%hash0;
+# push @arr0, \%hash1;
+
+# print "@arr\n";
+# print $arr[1]{'here'};
+
+# for my $h (@arr) {
+#   for my $key (keys %$h) {
+#     print %$h{$key}."\t";
+#   }
+# }
+
+# print "@arr0\n";
+# print $arr0[1]{'here'};
 
